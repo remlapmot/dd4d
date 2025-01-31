79556bc0542bd42a5fa0ba7229dc14fd2fffdbd4
Tom Palmer <remlapmot@hotmail.com>
2025-01-31 09:43:05 +0000

    Delete empty examples section

--- R/bn2dagitty.R
+++ R/bn2dagitty.R
@@ -4,7 +4,6 @@
 #'
 #' @return dagitty object
 #' @export
-#' @examples
 bn2dagitty <- function(bn_df){
   dagitty_str = purrr::map2_chr(bn_df$variable, bn_df$parents, ~paste0(.x, " <- ", "{", paste0(.y, collapse=" ") , "}"))
   dagitty::dagitty(paste0("dag {", paste0(dagitty_str, collapse=" "), "}"))
