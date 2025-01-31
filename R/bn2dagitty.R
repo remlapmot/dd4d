0e355a7fe1bd8d44c7467f6d190f5497b159dcf2
Tom Palmer <remlapmot@hotmail.com>
2025-01-31 08:57:40 +0000

    Delete whitespace

--- R/bn2dagitty.R
+++ R/bn2dagitty.R
@@ -1,13 +1,10 @@
-
 #' Converts a bn_df object to a dagitty object
 #'
 #' @param bn_df initialised bn_df object, with simulation instructions. Created with `bn_create`
 #'
 #' @return dagitty object
 #' @export
-#'
 #' @examples
-#'
 bn2dagitty <- function(bn_df){
   dagitty_str = purrr::map2_chr(bn_df$variable, bn_df$parents, ~paste0(.x, " <- ", "{", paste0(.y, collapse=" ") , "}"))
   dagitty::dagitty(paste0("dag {", paste0(dagitty_str, collapse=" "), "}"))
