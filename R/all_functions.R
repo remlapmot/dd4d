c6fbe6805b6b66af29a2c8d4daa099c7d0428294
Tom Palmer <remlapmot@hotmail.com>
2025-01-31 08:58:01 +0000

    Delete whitespace

--- R/all_functions.R
+++ R/all_functions.R
@@ -1,13 +1,10 @@
-
 #' Get all functions that are used in a formula  `expr`.
 #'
 #' @param expr a formula object
 #'
 #' @return A character vector with the extracted names.
 #' @export
-#'
 #' @examples
-#'
 all_funs = function(expr){
   all.names(expr, unique=TRUE)[!(all.names(expr, unique=TRUE) %in% all.vars(expr))]
 }
